f56142f23cc389b13f3c1f6168adad751dfac372
Salma Suliman <ssuliman91@gmail.com>
2018-03-01 15:54:48 -0800

    Initial commit.

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('form').submit(function() {
+        var fname = $('input[name="first_name"]').val();
+        var lname = $('input[name="last_name"]').val();
+        var description = $('textarea').val();
+        var newElement = $(
+            '<div><h1>' + fname + ' ' + lname + 
+            '</h1><h4>Click for description!</h4><p>' + description + 
+            '</p><p class="toggle">Click for full name.</p></div>'
+        );
+        $('#contact-list').append(newElement);
+        newElement.children('p').hide();
+        return false;
+    });
+    $(document).on("click", "#contact-list div", function() {
+        $(this).children('h1, h4').toggle();
+        $(this).children('p').toggle();
+    });
+});
